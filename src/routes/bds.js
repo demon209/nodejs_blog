870497817b720bcaa12b0d09e0681076666328c3
Wuan_26092004 <phamhuyen1115204@gmail.com>
2024-09-05 00:51:50 +0700

    Cập nhật router bds.js chỉ cho phép người dùng đã đăng nhập truy cập vào trang chi tiết bđs

--- src/routes/bds.js
+++ src/routes/bds.js
@@ -1,5 +1,11 @@
 const express = require('express')
 const router = express.Router()
 const bdsDetail = require('../app/controllers/bdsDetail');
-router.get("/bds/:id", bdsDetail.detail);
+function checkAuthenticated(req, res, next) {
+    if (req.session.user || req.path === '/forgot-password' || req.path === '/reset-password') {
+      return next(); // Cho phép truy cập nếu đã đăng nhập hoặc vào các trang cho phép không cần đăng nhập
+    }
+    res.redirect('/login'); // Chuyển hướng đến trang đăng nhập nếu không có session
+  }
+router.get("/bds/:id", checkAuthenticated,bdsDetail.detail);
 module.exports = router
